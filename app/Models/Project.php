a3523505d8730563bd96927e938377c272305d00
jacopo-derrico <derricoj.design@gmail.com>
2024-09-04 22:17:28 +0200

    feat(projects): index CRUD & fillable

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'date',
+        'description',
+        'cover_path',
+        'link',
+        'git_link'
+    ];
+
+    // autogenerate the slug from title
+    public static function generateSlug($title){
+        return Str::slug($title, '-');
+    }
 }
