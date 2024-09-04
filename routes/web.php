53090d7f541a48e0110ff5b3f181c88617add80d
jacopo-derrico <derricoj.design@gmail.com>
2024-09-04 22:15:08 +0200

    feat(projects): add routin to project controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// project views
+Route::resource('projects', ProjectController::class);
 
 require __DIR__.'/auth.php';
