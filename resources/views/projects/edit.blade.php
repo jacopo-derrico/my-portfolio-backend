9cdc006cd44cade6be69c90bebd29ba5eb174d2d
jacopo-derrico <derricoj.design@gmail.com>
2024-09-15 00:26:37 +0200

    fix: changed form action to PUT

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -14,8 +14,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form method="POST" action="{{ route('projects.store') }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('projects.update', $project->id) }}" enctype="multipart/form-data">
                         @csrf
+                        @method('PUT')
                         
                         <div class="flex w-full gap-3">
                             <!-- Title -->
@@ -74,4 +75,16 @@
             </div>
         </div>
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+        const coverPathInput = document.getElementById('cover_path');
+        
+        coverPathInput.addEventListener('change', function() {
+                if (!this.files || this.files.length === 0) {
+                    this.value = null;
+                }
+            });
+        });
+    </script>
 </x-app-layout>
