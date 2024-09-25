adf2ca3b457fda45048b436d7b184badfddf754c
jacopo-derrico <derricoj.design@gmail.com>
2024-09-25 17:24:41 +0200

    fix: images table accept null title & description

--- database/migrations/2024_09_24_102512_create_images_table.php
+++ database/migrations/2024_09_24_102512_create_images_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->string('image_path');
-            $table->string('title');
-            $table->text('description');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
